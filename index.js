568a5aacd35b480e12a45f30b1cd1755e23dbbb0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-05 11:44:56 +0530

    sbi node demo

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 
 console.log('node demo works!');
 
+// copied from - 
+// https://www.npmjs.com/package/express
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000)
