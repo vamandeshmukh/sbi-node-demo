9d0c9b5dca70ef1db5b808e980a5833ca2ee1144
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-05 11:53:58 +0530

    sbi node demo

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ console.log('node demo works!');
 // 2. edit package.json 
 // 3. run npm install on cmd 
 // 4. run on cmd npm start 
-
+// 5. view the output on browser - 
+// http://localhost:3000/
+// 6. to stop - 
+// Ctrl + C Y 
 
 const express = require('express')
 const app = express()
